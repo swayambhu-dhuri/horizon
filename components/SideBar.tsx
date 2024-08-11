f145f4a63665786657099bde29c2ea2c43cd1f71
swayambhu-dhuri <50697539+swayambhu-dhuri@users.noreply.github.com>
2024-08-11 10:23:17 -0400

    implement auth, appwrite, and sentry

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 const SideBar = ({ user }: SiderbarProps) => {
 	const pathname = usePathname();
 
@@ -54,12 +55,11 @@ const SideBar = ({ user }: SiderbarProps) => {
 							</p>
 						</Link>
 					);
-        })}
-        
-        USER
-      </nav>
-      
-      FOOTER
+				})}
+				USER
+			</nav>
+
+			<Footer user={user} />
 		</section>
 	);
 };
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { ID } from "node-appwrite";
+import { parseStringify } from "../utils";
+
+export const signIn = async ({ email, password }: signInProps) => {
+	try {
+		const { account } = await createAdminClient();
+		const session = await account.createEmailPasswordSession(
+			email,
+			password,
+		);
+		cookies().set("appwrite-session", session.secret, {
+			path: "/",
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+		});
+		return parseStringify(session);
+	} catch (error) {
+		console.error("Error signing in", error);
+	}
+};
+
+export const signUp = async (userData: SignUpParams) => {
+	const { email, password, firstName, lastName } = userData;
+	try {
+		const { account } = await createAdminClient();
+
+		const newUserAccount = await account.create(
+			ID.unique(),
+			email,
+			password,
+			`${firstName} ${lastName}`,
+		);
+		const session = await account.createEmailPasswordSession(
+			userData.email,
+			userData.password,
+		);
+
+		cookies().set("appwrite-session", session.secret, {
+			path: "/",
+			httpOnly: true,
+			sameSite: "strict",
+			secure: true,
+		});
+		return parseStringify(newUserAccount);
+	} catch (error) {
+		console.error("Error sign up", error);
+	}
+};
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+	try {
+		const { account } = await createSessionClient();
+		const user = await account.get();
+		return parseStringify(user);
+	} catch (error) {
+		return null;
+	}
+}
+
+export async function logoutAccount() {
+	try {
+		const { account } = await createSessionClient();
+		cookies().delete("appwrite-session");
+		await account.deleteSession("current");
+	} catch (error) {
+		return null;
+	}
+}
