9bfb2c687a178a9d2091e6ee9e61b5bb7108fa9d
swayambhu-dhuri <50697539+swayambhu-dhuri@users.noreply.github.com>
2024-08-12 08:45:10 -0400

    Implement plaid link and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
 	const router = useRouter();
@@ -43,8 +44,21 @@ const AuthForm = ({ type }: { type: string }) => {
 		setIsLoading(true);
 		try {
 			// Sign up with appwrite and create a plain link token
+
 			if (type === "sign-up") {
-				const newUser = await signUp(data);
+				const userData = {
+					firstName: data.firstName!,
+					lastName: data.lastName!,
+					address1: data.address1!,
+					city: data.city!,
+					state: data.state!,
+					postalCode: data.postalCode!,
+					dateOfBirth: data.dob!,
+					ssn: data.ssn!,
+					email: data.email,
+					password: data.password,
+				};
+				const newUser = await signUp(userData);
 				setUser(newUser);
 			}
 			if (type === "sign-in") {
@@ -95,7 +109,9 @@ const AuthForm = ({ type }: { type: string }) => {
 				</div>
 			</header>
 			{user ? (
-				<div className="flex flex-col gap-4">{/* Plaid Link */}</div>
+				<div className="flex flex-col gap-4">
+					<PlaidLink user={user} variant="primary" />
+				</div>
 			) : (
 				<>
 					<Form {...form}>
